2529c6976b10e74572c91a24faf7015f773d178f
Roman Sergeev <robespier@rambler.ru>
2017-07-28 18:04:13 +0300

    Static html collect with Grunt
    Express - в топку, Jade -> Pug. Все файлы (html, css, extension files)
    собираются Grunt-ом в папку dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,53 @@
 "use strict";
 
 module.exports = function(grunt) {
-  require("load-grunt-config")(grunt, {
-    data: {
-      ccVersion: "4",
-      pkg: grunt.file.readJSON("package.json"),
-    }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    pug: {
+      compile: {
+        options: {
+          pretty: true,
+          data: "src/html/index.json"
+        },
+        files: {
+          "dist/index.html": ["src/html/index.pug"]
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ";",
+      },
+      dist: {
+        src: ["src/css/**"],
+        dest: "dist/assets/styles.css",
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/extension/",
+            src: [".debug", "CSXS/**"],
+            dest: "dist/"
+          },
+        ],
+      },
+/*
+      deploy: {
+        expand: true,
+        cwd: "dist/",
+        src: "**",
+        dest: path.join(process.env.APPDATA, "Adobe/CEP/extensions", "<%= pkg.name %>")
+      }
+*/
+    },
   });
+
+  grunt.loadNpmTasks("grunt-contrib-pug");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask("default", ["pug", "concat", "copy"]);
 };
