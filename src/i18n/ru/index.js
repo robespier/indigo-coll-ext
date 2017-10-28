e8c6bb82165c34403604b8052c79dedd944f1bb4
Roman Sergeev <robespier@rambler.ru>
2017-10-28 13:35:57 +0300

    Extract rest sections more

--- src/i18n/ru/index.js
+++ src/i18n/ru/index.js
@@ -1,5 +1,10 @@
 module.exports = {
   t: {
+    template: 'Шаблон',
+    labels: 'Этикетки',
     roll: 'Намотка',
+    inks: 'Красочность',
+    hotfolder: 'Горячая папка',
+    type: 'Тип верстки',
   },
 };
